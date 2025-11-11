f7b2b7084a6b43395adef3adf9e9cc8c4a036fa8
genie525 <nlcc3333@naver.com>
2025-11-12 00:54:02 +0900

    ss

--- src/main/java/likelion13th/shop/login/controller/UserController.java
+++ src/main/java/likelion13th/shop/login/controller/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "우효하지 않음")
 
     })
-    @PostMapping("reissue")
+    @PostMapping("/reissue")
     public ApiResponse<JwtDto> reissue(HttpServletRequest request) {
         try {
             JwtDto jwt = userService.reissue(request);
@@ -52,7 +52,7 @@ public class UserController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "우효하지 않음")
 
     })
-    @DeleteMapping("/user/logout")
+    @DeleteMapping("/users/logout")
     public ApiResponse<Void> logout(HttpServletRequest request) {
         userService.logout(request);
         return ApiResponse.onSuccess(SuccessCode.USER_LOGOUT_SUCCESS, null);
